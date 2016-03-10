b5fe0145a570d77c500aa0f3fe0135a9dea635e9
Alexander-Wen <alex@alexanderwen.me>
2016-03-10 00:59:24 -0500

    add test post

--- app/routes/template/template.controller.js
+++ app/routes/template/template.controller.js
@@ -5,3 +5,11 @@ exports.getTest = function *() {
   // result sent back to frontend
   this.body =  {'result': 'get test succeeded'};
 }
+
+exports.postTest = function *() {
+  const content = this.request.body;
+
+  console.log(content);
+  // result sent back to frontend
+  this.body =  {'result': 'post test succeeded'};
+}
--- app/routes/template/template.router.js
+++ app/routes/template/template.router.js
@@ -7,5 +7,6 @@ const template = require('./template.controller');
 const app = koa();
 
 app.use(router.get('/get-test', template.getTest));
+app.use(router.post('/post-test', template.postTest));
 
 module.exports = app;
